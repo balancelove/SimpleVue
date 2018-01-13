e311f4a6be2d91fd632cbc62de6fc382a05b7fdf
balancelove <1091598365@qq.com>
2018-01-13 11:08:17 +0800

    Create dep.js

---
+++ vue/dep.js
@@ -0,0 +1,15 @@
+class Dep {
+  constructor() {
+    this.subs = [];
+  }
+
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+
+  notify() {
+    this.subs.forEach(sub => sub.update());
+  }
+}
+
+Dep.target = null;
